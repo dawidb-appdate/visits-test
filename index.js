b383a20ae3e3a7226cd4cfde703ac65240df1392
Dawid Bury <dawidm.bury@codahead.com>
2019-08-07 20:58:15 +0200

    hardcoded redis host

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-/*
+
 const express = require('express');
 const redis = require('redis');
 
 const app = express();
 const client = redis.createClient({
-  host: 'redis',
+  host: 'redis-singlevisits.sid301.0001.use2.cache.amazonaws.com',
   potr: 6379
 });
 client.set('visits', 0);
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 app.listen(8081, () => {
   console.log('Listening on port 8081');
 });
-*/ 
+/* 
 const express = require('express');
 const app = express();
 
@@ -30,3 +30,4 @@ app.get('/', (req, res) => {
 app.listen(8081, () => {
   console.log('Listening on port 8081');
 });
+*/
